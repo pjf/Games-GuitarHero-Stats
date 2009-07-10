ce030eb06bd6c1b919002435858138a2394c60ab
Paul Fenwick <pjf@perltraining.com.au>
2009-07-10 12:03:41 +1000

    Module::Starter skeleton

---
+++ lib/Games/GuitarHero/Stats.pm
@@ -0,0 +1,107 @@
+package Games::GuitarHero::Stats;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Games::GuitarHero::Stats - The great new Games::GuitarHero::Stats!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Games::GuitarHero::Stats;
+
+    my $foo = Games::GuitarHero::Stats->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Paul Fenwick, C<< <pjf at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-games-guitarhero-stats at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Games-GuitarHero-Stats>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Games::GuitarHero::Stats
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Games-GuitarHero-Stats>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Games-GuitarHero-Stats>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Games-GuitarHero-Stats>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Games-GuitarHero-Stats>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Paul Fenwick, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Games::GuitarHero::Stats
---
+++ t/00-load.t
@@ -0,0 +1,9 @@
+#!perl -T
+
+use Test::More tests => 1;
+
+BEGIN {
+	use_ok( 'Games::GuitarHero::Stats' );
+}
+
+diag( "Testing Games::GuitarHero::Stats $Games::GuitarHero::Stats::VERSION, Perl $], $^X" );
